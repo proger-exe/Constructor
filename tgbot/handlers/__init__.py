10ba8b4fa6373e5850064f4d67b9d70698edd79f
Sh3lby <70023601+proger-exe@users.noreply.github.com>
2025-10-24 07:50:05 +0500

    Handle missing Vault secrets gracefully

--- tgbot/handlers/__init__.py
+++ tgbot/handlers/__init__.py
@@ -1,3 +1,11 @@
+from .admin import admin_router
 from .tenant.admin import admin_router as tenant_admin_router
 from .tenant.user import user_tenant_router
 from .user import user_router
+
+__all__ = (
+    "admin_router",
+    "tenant_admin_router",
+    "user_tenant_router",
+    "user_router",
+)
